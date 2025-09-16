dd7d473013f34c9ed503907f63c966bd0e58e226
Prajwal L K Urs <153601054+iprajwallkurs@users.noreply.github.com>
2025-09-16 14:46:38 +0530

    Created Location permission Screen

--- app/src/main/java/com/example/location/MainActivity.kt
+++ app/src/main/java/com/example/location/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.location
 
+import android.Manifest
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.location.ui.theme.LocationTheme
@@ -20,9 +29,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             LocationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -31,17 +37,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LocationTheme {
-        Greeting("Android")
+fun LocationDisplay(
+    locationUtils: LocationUtils,
+    context: Context
+){
+
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = { permissions ->
+            if (permissions[Manifest.permission.ACCESS_COARSE_LOCATION]== true
+                && permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true){
+                //HAVE ACESS TO LOCATION
+            }else{
+                //Ask for Permission
+            }
+        })
+
+
+
+    Column (modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center){
+
+        Text("Location not available")
+
+        Button(onClick = {
+            if (locationUtils.hasLocationPermission(context)){
+                // Permission already granted update the location
+            }else{
+                //Request location permission
+            }
+        }) {
+            Text("Get Location")
+        }
     }
 }
