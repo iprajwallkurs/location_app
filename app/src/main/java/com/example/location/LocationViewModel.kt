d5bab39070fadd62a543e8ebad6192dc2699c61a
Prajwal L K Urs <153601054+iprajwallkurs@users.noreply.github.com>
2025-10-27 21:38:11 +0530

    Modified LocationViewModel.kt file

--- app/src/main/java/com/example/location/LocationViewModel.kt
+++ app/src/main/java/com/example/location/LocationViewModel.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.State
 
 import androidx.lifecycle.ViewModel
 
-
 class LocationViewModel : ViewModel() {
     private val _location = mutableStateOf<LocationData?>(null)
     val location : State<LocationData?> = _location
